7329543e736aac3b6025285a7697f84cf9a90b47
rezaantika03 <38091165+rezaantika03@users.noreply.github.com>
2018-11-20 13:54:42 +0700

    Add files via upload

---
+++ assets5/kelahiran.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>pabw.go.id</title>
+	<link rel="stylesheet" href="http://localhost/ciblog/assets5/style.css">
+</head>
+<body>
+	<div class="logo">
+					<h1><img src="http://localhost/ciblog/assets5/image/logo.png" width="400px"></h1>
+		</div>
+	<div class="bg_content">
+		<div class="text">
+		<br><br><h2 align="center">AKTA KELAHIRAN</h2>
+		<p align="center">Selasa, 31 Oktober 2018 01:45 WIB</p></br>
+		<h2>Pelayanan Pelaporan Kelahiran</h2></br>
+		<h2>Lokasi Pelayanan : Kantor Kelurahan</h2>
+		<h2>Waktu Pelayanan : 5 hari kerja sejak tanggal diterimanya berkas persyaratan secara lengkap
+		</h2>
+		<h2>Biaya : Gratis</h2>
+		<p>Sesuai dengan Undang-Undang No. 24 Tahun 2013 tentang Perubahan atas Undang-Undang No. 23 Tahun 2006 pencatatan kelahiran yang sebelumnya berdasarkan atas asa peristiwa, sejak ditetapkannya undang-undang ini berubah menjadi berdasarkan atas domisili, sehingga pencatatan dilakukan pada instansi pelaksanan sesuai dengan domisili pelapor. </p>
+
+		<p>Bayi yang dilaporkan kelahirannya akan terdaftar dalam Kartu Keluarga dan diberi Nomor Induk Kependudukan (NIK) sebagai dasar untuk memperoleh pelayanan masyarakat lainnya. 
+Sebagai hasil pelaporan kelahiran, diterbitkan Kartu Keluarga dan Akta Kelahiran.</p>
+
+<h2>Persyaratan</h2>
+<p>Untuk memperoleh Pelayanan Pelaporan Kelahiran harus memenuhi persyaratan berikut ini:</br>
+a. Surat Pengantar RT/RW; </br>
+b. Surat Keterangan Kelahiran dari Rumah Sakit/Dokter/Bidan/Pilot/Nahkoda; </br>
+c. Asli dan Fotokopi KK bagi penduduk/SKSKPNP bagi penduduk non permanen; </br>
+d. Asli dan Fotokopi KTP Orang tua/SKDS/Surat Keterangan Pelaporan Tamu; </br>
+e. Asli dan Fotokopi Surat Nikah/Akta Perkawinan Orang tua; </br>
+f. Asli dan Fotokopi Paspor bagi Orang Asing; </br>
+g. Surat Keterangan Kepolisian untuk anak yang tidak diketahui asal-usulnya; dan </br>
+h. Surat Keterangan dari lembaga sosial untuk kelahiran anak penduduk rentan.</p>
+
+<h2>Pelayanan Penerbitan Akta Kelahiran</h2>
+<h2>Lokasi Pelayanan : Suku Dinas Kependudukan dan Catatan Sipil Kotamadya</h2>
+<h2>Waktu Pelayanan : 5 hari kerja sejak tanggal diterimanya berkas persyaratan secara lengkap
+</h2>
+<h2>Biaya : Gratis</h2></br>
+
+<h2>Akta Kelahiran </h2><p>adalah Akta Catatan Sipil hasil pencatatan peristiwa kelahiran seseorang.</p><br>
+<h2 align="center">Borang Akta Kelahiran</h2>
+
+					<h1><img src="http://localhost/ciblog/assets5/image/b.jpg" width="200px"></h1>
+	
+
+<h2>Blangko Register Akta Kelahiran</h2>
+<h2>Persyaratan</h2>
+<p>Sangat disarankan mengurus akta kelahiran sesegera mungkin setelah bayi dilahirkan. Adapun persyaratan untuk membuat akta kelahiran adalah sebagai berikut :<br> 
+a. Surat Keterangan Kelahiran dari Kelurahan<br>
+b. Asli dan Fotokopi Surat Keterangan Kelahiran dari dokter/bidan/penolong kelahiran/Nakhoda Kapal Laut atau Pilot Pesawat Terbang dengan memperlihatkan aslinya Surat<br>
+c. Nikah/Akta Perkawinan orang tua<br>
+d. Fotokopi KK dan KTP orang tua<br>
+e. Nama dan identitas saksi pelaporan kelahiran<br>
+f. Persetujuan Kepala Dinas/Suku Dinas. dalam hal pelaporannya melebihi 60 (enam puluh) hari sejak tanggal kelahirannya, dan<br>
+Berdasarkan  putusan Mahkamah Konstitusi Nomor 18 /PUU-XI/2013 tanggal 14 Maret 2013, ketentuan tentang batas waktu 1 (satu) tahun dan persyaratan penetapan pengadilan yang diisyaratkan dalam pasal 32 Undang-Undang Nomor 23 Tahun 2006 dibatalkan. Dengan demikian pencatatan kelahiran yang melampaui batas 1 (satu) tahun sejak tanggal kelahiran tidak melalui penetapan pengadilan. <br>
+Berdasarkan Undang-Undang No. 24 tahun 2013 tentang Perubahan atas Undang-Undang No. 23 Tahun 2006 tentang Administrasi Kependudukan bahwa Pencatatan kelahiran wajib dilaporkan oleh penduduk pada Instansi pelaksana tempat terjadinya peristiwa diubah menjadi wajib dilaporkan oleh penduduk pada Instansi pelaksana tempat penduduk berdomisili.</p><br>
+
+<h2>Jenis Akta Kelahiran</h2>
+
+<p>Akta kelahiran digolongkan menurut jarak waktu pelaporan dengan kelahiran. Ada  jenis-jenis akta kelahiran, yaitu :<br>
+
+1. Akta Kelahiran Umum Akta kelahiran yang dibuat berdasarkan laporan kelahiran yang disampaikan dalam batas waktu selambat-lambatnya 60 (enam puluh) hari kerja bagi WNI dan 10 (sepuluh) hari kerja bagi WNA sejak tanggal kelahiran bayi.<br>
+2. Akta Dengan Rekomendasi Akta Kelahiran yang dibuat berdasarkan Rekomendasi kepala Dinas atas laporan kelahiran yang telah melampaui batas waktu 60 (enam puluh) hari kerja.</p>
+
+
+
+	
+	</div>
+	</div>
+
+	
+</body>
+</html>
---
+++ assets5/kk.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>pabw.go.id</title>
+	<link rel="stylesheet" href="http://localhost/ciblog/assets5/style.css">
+</head>
+<body>
+	<div class="logo">
+					<h1><img src="http://localhost/ciblog/assets5/image/logo.png" width="400px"></h1>
+		</div>
+	<div class="bg_content">
+		<div class="text">
+		<br><br><h2 align="center">KARTU KELUARGA (KK)</h2>
+		<p align="center">Selasa, 31 Oktober 2018 01:45 WIB</p></br>
+		<h2>Lokasi Pelayanan : Kantor Kelurahan</h2>
+		<h2>Waktu Pelayanan : 14 hari kerja sejak tanggal diterimanya berkas persyaratan secara lengkap
+</h2>
+		<h2>Biaya : Gratis</h2>
+		<p>Kartu Keluarga adalah Kartu Identitas Keluarga yang memuat data tentang susunan, hubungan dan jumlah anggota keluarga.
+Kartu Keluarga wajib dimiliki oleh setiap keluarga. Kartu ini berisi data lengkap tentang identitas Kepala Keluarga dan anggota keluarganya.<br>
+ <h2 align="center">Contoh Kartu Keluarga</h2>
+
+					<h1><img src="http://localhost/ciblog/assets5/image/d.png" width="500px"></h1>
+	
+Kartu keluarga dicetak rangkap 4 yang masing-masing dipegang oleh :<br>
+a. Kepala Keluarga (Iembar pertama);<br>
+b. Ketua Rukun Tetangga (Iembar kedua).<br>
+c. Lurah (Iembar ketiga) ; dan<br>
+d. Suku Dinas (Iembar keempat).<br><br>
+ 
+<h2>Perubahan Data</h2>
+ 
+Setiap terjadi perubahan data dalam Kartu Keluarga seperti karena terjadi peristiwa Kelahiran, Kematian, Kepindahan, dll, Kepala Keluarga wajib melaporkan ke kelurahan dengan persyaratan<br>
+a. Surat Pengantar RT/RW<br>
+b. KK lama<br>
+c. Asli dan Fotokopi Surat Keterangan Kematian/Kutipan Akta Kematian;<br>
+d. Asli dan Fotokopi Akta Perkawinan/Perceraian bagi yang pernah menikah/ bercerai<br>
+e. Surat Keterangan Pindah bagi penduduk yang pindah, dan<br>
+f. Asli dan Fotokopi Kutipan Akta Perceraian, bagi pengurangan anggota keluarga karena cerai.<br>
+Dari hasil perlaporan tersebut akan diterbitkan Kartu Keluarga baru.<br><br>
+ 
+<h2>Kepindahan</h2>
+ 
+Apabila suatu keluarga pindah seluruhnya ke tempat lain, maka Kartu Keluarga yang disimpan di Kepala Keluarga dan di Ketua RT harus diserahkan kepada Lurah (dicabut). Di tempat tinggal yang baru, berdasarkan Surat Keterangan Pindah, Lurah akan memberi Kartu Keluarga yang baru.<br><br>
+ 
+<h2>Persyaratan Pembuatan KK</h2>
+ 
+Untuk membuat Kartu Keluarga harus melengkapi syarat-syarat berikut: <br>a. Surat Pengantar RT/RW<br>
+b. Biodata penduduk<br>
+c. KK lama<br>
+d. Asli dan Fotokopi Kutipan Akta Perkawinan/Akta Nikah<br>
+e. Asli dan Fotokopi Izin Tinggal Tetap bagi Orang Asing<br>
+f. Surat Keterangan Pindah/Surat Keterangan Pindah Datang (SKP/SKPD) dan<br>
+g. Surat Keterangan Datang dari Luar Negeri (SKDLN) bagi WNI yang datang dari luar negeri.<br><br>
+ 
+<h2>Perhatian</h2>
+ 
+Kartu Keluarga (KK) adalah Dokumen milik Pemda Propinsi DKI Jakarta dan karena itu tidak boleh mencoret, merubah, mengganti, menambah isi data yang tercantum dalam Kartu Keluarga.
+Setiap terjadi perubahan karena Mutasi Data dan Mutasi Biodata, wajib dilaporkan kepada Lurah dan akan diterbitkan Kartu Keluarga (KK) yang baru<br>
+Pendatang baru yang belum mendaftarkan diri atau belum berstatus penduduk DKI Jakarta, nama dan identitasnya tidak boleh dicantumkan dalan Kartu Keluarga.
+
+</p>
+
+</body>
+</html>
